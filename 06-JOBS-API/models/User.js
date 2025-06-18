3cc5b336dcd8eb744e14f69fbfb261448dbf7d52
Mangal <dasmann90@gmail.com>
2025-06-18 12:38:15 +0530

    login

--- 06-JOBS-API/models/User.js
+++ 06-JOBS-API/models/User.js
@@ -38,4 +38,9 @@ UserSchema.methods.createJWT = function () {
   );
 };
 
+UserSchema.methods.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
+};
+
 module.exports = mongoose.model("User", UserSchema);
